79efc59a870bf6dff54d11c1ce6c4545b11ae3e2
samarafsantos <samarasantos@poli.ufrj.br>
2021-02-04 01:13:06 -0300

    Mocked courses info to enable layout adjustments

---
+++ src/pages/Course.js
@@ -0,0 +1,53 @@
+import axios from 'axios';
+import React, { useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import { courses } from '../utils/mockedCourses';
+
+export default function Course() {
+  const [course, setCourse] = useState([]);
+  const { id } = useParams();
+  useEffect(()=>{
+    setCourse(courses.filter(course => course.id === parseInt(id)));
+  }, []);
+  
+  console.log(course);
+  return (
+    <Container>
+        <HeaderTemporario>Temporário</HeaderTemporario>
+        {course.length !== 0 && 
+            <Details>
+                <h1>{course[0].title}</h1>
+                <p>{course[0].description}</p>
+            </Details>
+        }
+    </Container>
+  );
+}
+
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-color: #E5E5E5;
+`;
+
+const HeaderTemporario = styled.header`
+    height: 90px;
+    background-color: #FFFFFF;
+`;
+const Details = styled.div`
+    height: 180px;
+    background: linear-gradient(180deg, #EFDA4F 0%, rgba(239, 218, 79, 0.56) 100%);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 20px;
+    h1{
+        color: #000;
+        font-size: 35px;
+    }
+    p{
+        color: #000;
+        font-size: 20px;
+    }
+`;
---
+++ src/utils/mockedCourses.js
@@ -0,0 +1,24 @@
+export const courses = [
+  {
+    id: 1,
+    title: 'JavaScript para Iniciantes',
+    description: 'Aprenda JavaScript do zero hoje!',
+    icon: 'https://cdn.icon-icons.com/icons2/2108/PNG/512/javascript_icon_130900.png',
+    background: 'https://image.freepik.com/fotos-gratis/antigas-cinzento-cimento-parede-fundos_34552-324.jpg',
+  },
+  {
+    id: 2,
+    title: 'React para Iniciantes',
+    description: 'Aprenda React do zero hoje!',
+    icon: 'https://cdn.icon-icons.com/icons2/2108/PNG/512/javascript_icon_130900.png',
+    background: 'https://image.freepik.com/fotos-gratis/antigas-cinzento-cimento-parede-fundos_34552-324.jpg',
+  },
+  {
+    id: 3,
+    title: 'NodeJs para Iniciantes',
+    description: 'Aprenda NodeJs do zero hoje!',
+    icon: 'https://cdn.icon-icons.com/icons2/2108/PNG/512/javascript_icon_130900.png',
+    background: 'https://image.freepik.com/fotos-gratis/antigas-cinzento-cimento-parede-fundos_34552-324.jpg',
+  },
+];
+
