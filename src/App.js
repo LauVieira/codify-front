c5588b839df6164f1364586ebc19c88b33c0554c
Gabriel Milhomem <gabriell.mil@gmail.com>
2021-02-05 00:25:27 -0300

    Protected Routes and Unprotected Routes with cookies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { CookiesProvider, Cookies } from 'react-cookie';
+import { 
+  BrowserRouter as Router, 
+  Route, 
+  Switch, 
+  Redirect 
+} from 'react-router-dom';
 
 import GlobalStyle from './assets/GlobalStyles';
 import { UserProvider } from './contexts/UserContext';
@@ -8,17 +14,47 @@ import * as Pages from './pages';
 
 export default function App() {
   return (
+    <CookiesProvider>
       <UserProvider>
         <Router>
           <GlobalStyle />
           <Switch>
-            <Route path="/cadastrar" component={Pages.SignUp} />
-            <Route path="/entrar" component={Pages.SignIn} />
-            <Route path="/esqueci-senha" component={Pages.ForgotPassword} />
-            <Route path="/redefinir-senha" component={Pages.RedefinePassword} />
-            <Route path="/" exact component={Pages.Home} />
+            <UnprotectedRoute path="/cadastrar" component={Pages.SignUp} />
+            <UnprotectedRoute path="/entrar" component={Pages.SignIn} />
+            <UnprotectedRoute path="/esqueci-senha" component={Pages.ForgotPassword} />
+            {/*<Route path="/redefinir-senha" component={Pages.RedefinePassword} />*/}
+            <ProtectedRoute path="/" exact component={Pages.Home} />
           </Switch>
         </Router>
       </UserProvider>
+    </CookiesProvider>
+  );
+}
+
+function ProtectedRoute(props) {
+  const token = Cookies.get('token');
+
+  if(!token) {
+    return (
+      <Redirect to='/entrar' />
+    );
+  }
+
+  return (
+    <Route {...props} />
+  );
+}
+
+function UnprotectedRoute(props) {
+  const token = Cookies.get('token');
+
+  if(token) {
+    return (
+      <Redirect to='/'/>
+    );
+  }
+
+  return (
+    <Route {...props} />
   );
 }
