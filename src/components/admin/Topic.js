a42516b261ecf6e0bb583a5a65b8541895a83b70
samarafsantos <samarasantos@poli.ufrj.br>
2021-02-12 16:19:55 -0300

    topics and chapters implemented

---
+++ src/components/admin/Topic.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {
+  List,
+  Create,
+  Edit,
+  Datagrid,
+  TextField,
+  EditButton,
+  DeleteButton,
+  SimpleForm,
+  TextInput,
+  ReferenceField,
+  ReferenceInput,
+  SelectInput,
+} from 'react-admin';
+
+export const TopicList = (props) => (
+  <List {...props}>
+    <Datagrid rowClick="edit">
+      <TextField source="id" />
+      <TextField source="title" />
+      <ReferenceField source="chapterId" reference="chapters" label="Capítulo">
+        <TextField source="title" />
+      </ReferenceField>
+      <EditButton basePath="/topics" />
+      <DeleteButton basePath="/topics" />
+    </Datagrid>
+  </List>
+);
+
+export const TopicCreate = (props) => (
+  <Create {...props}>
+    <SimpleForm>
+      <TextInput disabled source="id" />
+      <TextInput source="title" />
+      <ReferenceInput source="chapterId" reference="chapters" label="Capítulo">
+        <SelectInput optionText="title" />
+      </ReferenceInput>
+      <DeleteButton basePath="topics" />
+    </SimpleForm>
+  </Create>
+);
+
+export const TopicEdit = (props) => (
+  <Edit {...props}>
+    <SimpleForm>
+      <TextInput disabled source="id" />
+      <TextInput source="title" />
+      <ReferenceInput source="chapterId" reference="chapters" label="Capítulo">
+        <SelectInput optionText="title" />
+      </ReferenceInput>
+      <DeleteButton basePath="topics" />
+    </SimpleForm>
+  </Edit>
+);
--- src/pages/Admin/Dashboard.js
+++ src/pages/Admin/Dashboard.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Admin, Resource, fetchUtils } from 'react-admin';
 import simpleRestProvider from 'ra-data-simple-rest';
 import { CourseList, CourseCreate, CourseEdit } from '../../components/admin/Course';
+import { ChapterList, ChapterCreate, ChapterEdit } from '../../components/admin/Chapter';
+import { TopicList, TopicCreate, TopicEdit } from '../../components/admin/Topic';
 import authProvider from './authProvider';
 
 const httpClient = (url, options = {}) => {
@@ -16,7 +18,9 @@ const dataProvider = simpleRestProvider('http://localhost:3000/admin', httpClien
 export default function Dashboard() {
   return (
     <Admin authProvider={authProvider} dataProvider={dataProvider}>
-      <Resource name="courses" list={CourseList} create={CourseCreate} edit={CourseEdit}/>
+      <Resource name="courses" list={CourseList} create={CourseCreate} edit={CourseEdit} />
+      <Resource name="chapters" list={ChapterList} create={ChapterCreate} edit={ChapterEdit} />
+      <Resource name="topics" list={TopicList} create={TopicCreate} edit={TopicEdit} />
     </Admin>
   );
 }
